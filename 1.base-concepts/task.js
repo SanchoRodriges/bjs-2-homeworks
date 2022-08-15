53745e3ba03f9469bfe9d4bcdecee03742d327a8
Sancho Rodriges <softilla@ya.ru>
2022-08-15 19:21:34 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,32 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b**2-4*a*c;
+  arr.push(d);
+  if (d > 0) {
+    let firstcalc = (-b + Math.sqrt(d) )/(2*a);
+    let secondcalc = (-b - Math.sqrt(d) )/(2*a);
+    arr = [firstcalc, secondcalc];
+  } else if (d === 0) {
+    let calc = -b/(2*a);
+    arr = [calc];
+  } else {
+    arr = [];
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (amount < contribution) 
+    totalAmount = 'общая стоимость не может быть меньше начального взноса';
+  
+  let now = new Date();
+  let creditDate = new Date(date);
+  totalAmount += now;
+  totalAmount += creditDate;
 
   return totalAmount;
 }
