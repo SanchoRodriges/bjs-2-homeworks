cdfd5d994abc2bb377cd7c2965ce2c629a311d69
Sancho Rodriges <softilla@ya.ru>
2022-08-25 19:36:26 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+
+  this.subject = subjectName;
+
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (addMark) {
+
+  if (this.marks === undefined) {
+    this.marks = [addMark];
+    } else {
+      this.marks.push(addMark);
+  }
+  
+}
+
+Student.prototype.addMarks = function (...data) {
+
+  if (this.marks === undefined) {
+    this.marks = [...data];
+    } else {
+      this.marks = this.marks.concat(data);
+  }
+  
+}
+
+Student.prototype.getAverage = function () {
+
+  marks = this.marks;
+  return (marks.reduce(( acc, item ) => acc + item, 0)) / marks.length;
+
+}
+
+Student.prototype.exclude = function (reason) {
+
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+  
+}
