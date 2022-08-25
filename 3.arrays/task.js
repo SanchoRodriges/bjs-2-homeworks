a103e46d6946f28e6f452d2f9ef68ddcc9c483c0
Sancho Rodriges <softilla@ya.ru>
2022-08-25 15:16:35 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((number, index) => number === arr2[index]);
+  } else {
+    result = false;
+  } 
 
   return result; // boolean
 }
@@ -9,7 +13,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(number => number > 0 && number %3 === 0).map(number => number * 10)
 
   return resultArr; // array
 }
