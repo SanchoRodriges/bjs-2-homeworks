b583606f23d3efafa96d17f52012d4535849f6ad
Sancho Rodriges <softilla@ya.ru>
2022-09-19 22:25:45 +0300

    Initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+  return (...args) => {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      return 'Из кэша: ' + objectInCache.result;
+    }
+
+    const result = func(...args);
+    
+    cache.push({hash, result});
+    if (cache.length > 5) { 
+      cache.splice(0, 1);
+    }
+    return 'Вычисляем: ' + result;
+  }
 }
 
+function debounceDecoratorNew(func, delay){
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  let timeoutId = null;
+  func.count = 0;
+  func.allCount = 0;
+
+  return function(...args) {
+    
+    func.allCount++;
+    
+    if (!func.allCount) {
+      func(...args);
+      func.count++;
+    }    
+    
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    
+      timeoutId = setTimeout(() => {
+      timeoutId = null;
+      func(...args);
+      func.count++;
+    }, delay);
+  }
 }
